26fe6679fc4b7966dfaa38506b1ed4485c02e39a
Dan Gautsch <dan.gautsch@thinkbrownstone.com>
2015-11-03 12:07:05 -0500

    Basic setup for es2015 demo

---
+++ index.js
@@ -0,0 +1,33 @@
+/* jshint esnext:true */
+
+// Classes
+class Car {
+	constructor(color, type) {
+		this.wheels = 4;
+		this.attributes = {
+			wheels: 4,
+			color: color,
+			type: type
+		};
+	}
+}
+
+
+var simpleCar = new Car('blue', 'classic coupe');
+var simpleTruck = new Car('red', 'truck');
+
+console.log(simpleCar);
+console.log(simpleTruck);
+
+// Let Variables and Scope
+
+// Constants
+
+
+// Extending Classes
+
+// Static class methods
+
+// Arrow Functions
+
+// Destructuring
