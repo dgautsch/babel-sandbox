a3ada9408816da803a1ad52241ea186c435d475e
Dan Gautsch <dan.gautsch@thinkbrownstone.com>
2015-11-05 11:51:52 -0500

    Setting up conference structure with no code

--- index.js
+++ index.js
@@ -1,67 +1,15 @@
 /* jshint esnext:true */
 
+// Arrows and Lexical This
+
 // Classes
-class Car {
-	constructor(color, type) {
-		this.wheels = 4;
-		this.attributes = {
-			wheels: 4,
-			color: color,
-			type: type
-		};
-	}
-
-	getAttributes() {
-		console.log('This ' + this.attributes.type + ' has the following attributes: ');
-		console.dir(this);
-	}
-}
-
-
-var simpleCar = new Car('blue', 'classic coupe');
-var simpleTruck = new Car('red', 'truck');
-
-simpleCar.getAttributes();
-simpleTruck.getAttributes();
 
 // Extending Classes
-class MegaCar extends Car {
-	constructor(color, type) {
-		super(color, type);
-		this.supercharged = true;
-	}
-	goFaster() {
-		console.log('Vroom!!!!');
-	}
-}
 
-var bugatti = new MegaCar('red', 'really fast car');
+// Let Variables and Constants
 
-bugatti.getAttributes();
-bugatti.goFaster();
-
-// Let Variables and Scope
-var myVariable = 'something else';
-function letTest() {
-	let myVariable = 'something';
-	console.log(myVariable);
-}
-
-letTest();
-console.log(myVariable);
-
-// Constants
-const API_KEY = "aaas9df89js*#*h8sdfsdfkj";
-
-// API_KEY = ":SLKDFJ:LSKDFJ";
-
-// Static class methods
-
-
-
-
-
-
-// Arrow Functions
+// Template Strings
 
 // Destructuring
+
+// Default Parameters
