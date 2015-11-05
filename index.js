1cd27373c9461f095a6da7e6b7f1cc6acdaffb7b
Dan Gautsch <dan.gautsch@thinkbrownstone.com>
2015-11-05 11:30:25 -0500

    Adding babel config to package.json

--- index.js
+++ index.js
@@ -10,23 +10,57 @@ class Car {
 			type: type
 		};
 	}
+
+	getAttributes() {
+		console.log('This ' + this.attributes.type + ' has the following attributes: ');
+		console.dir(this);
+	}
 }
 
 
 var simpleCar = new Car('blue', 'classic coupe');
 var simpleTruck = new Car('red', 'truck');
 
-console.log(simpleCar);
-console.log(simpleTruck);
-
-// Let Variables and Scope
-
-// Constants
-
+simpleCar.getAttributes();
+simpleTruck.getAttributes();
 
 // Extending Classes
+class MegaCar extends Car {
+	constructor(color, type) {
+		super(color, type);
+		this.supercharged = true;
+	}
+	goFaster() {
+		console.log('Vroom!!!!');
+	}
+}
+
+var bugatti = new MegaCar('red', 'really fast car');
+
+bugatti.getAttributes();
+bugatti.goFaster();
+
+// Let Variables and Scope
+var myVariable = 'something else';
+function letTest() {
+	let myVariable = 'something';
+	console.log(myVariable);
+}
+
+letTest();
+console.log(myVariable);
+
+// Constants
+const API_KEY = "aaas9df89js*#*h8sdfsdfkj";
+
+// API_KEY = ":SLKDFJ:LSKDFJ";
 
 // Static class methods
+
+
+
+
+
 
 // Arrow Functions
 
